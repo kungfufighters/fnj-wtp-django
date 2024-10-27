5b4ea7263bf165de80a52ae390936675550ef520
Gustavo Pajuelo <gp373@njit.edu>
2024-10-26 22:03:54 -0400

    Create Opportunities (removed user auth, not yet working)

--- wheretoplayApp/views.py
+++ wheretoplayApp/views.py
@@ -71,11 +71,10 @@ class LoginView(APIView):
 
 class OpportunityCreateView(APIView):
     # Only authenticated users can create opportunities
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated] (not yet working)
     def post(self, request):
         serializer = OpportunitySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
