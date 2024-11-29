140d74d7194928588cafbc7944c470382dd66608
JoeyMucci <jmucci314@gmail.com>
2024-11-29 15:51:35 -0500

    web socket stuff

--- wheretoplay/routing.py
+++ wheretoplay/routing.py
@@ -4,6 +4,6 @@ from wheretoplayApp.consumers import VotingConsumer  # Import the WebSocket cons
 
 # Define WebSocket URL patterns
 websocket_urlpatterns = [
-    re_path(r'ws/vote/(?P<code>\d{5})/$', VotingConsumer.as_asgi()),
+    re_path(r'ws/vote/(?P<code>\.{6})/$', VotingConsumer.as_asgi()),
     #re_path(r'ws/test/$', SurveyConsumer.as_asgi()),  # WebSocket route for /ws/test/
 ]
