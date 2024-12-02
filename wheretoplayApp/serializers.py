b96a5dc1e99c5318df40eb29ec09f128381fc09e
EngineNeo <neomaralit@gmail.com>
2024-12-02 01:54:01 -0500

    Serializer bug fix

--- wheretoplayApp/serializers.py
+++ wheretoplayApp/serializers.py
@@ -30,28 +30,25 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
 '''
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = User  # Assuming you're using Django's built-in User model
-        fields = ('username', 'email', 'password', 'password2')
+        model = User
+        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name')
 
-    # Validate that the passwords match
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
         return attrs
 
-    # Create the user
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
-        # Set the user's password securely
         user.set_password(validated_data['password'])
         user.save()
         return user
