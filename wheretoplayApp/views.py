b1e9882dd3cc703f79524c962f76994d8ebc7e39
JoeyMucci <jmucci314@gmail.com>
2024-12-04 16:35:29 -0500

    fixed invite links 3

--- wheretoplayApp/views.py
+++ wheretoplayApp/views.py
@@ -386,7 +386,7 @@ class SendInviteEmailView(APIView):
                 return Response({'error': 'You are not the owner of this workspace'}, status=status.HTTP_403_FORBIDDEN)
 
             # Generate unique token for the invitation
-            token = uuid.uuid4()
+            # token = uuid.uuid4() 
 
             # Create invitation
             invitation = Invitation.objects.create(
@@ -400,7 +400,9 @@ class SendInviteEmailView(APIView):
             # Generate invite link with token
             #protocol = 'https' if request.is_secure() else 'http'
             #domain = request.get_host()
-            invite_link = f"https://www.where2play.net/join/{token}/"
+            #invite_link = f"https://www.where2play.net/join/{token}/"
+            # We can take them directly to voting they get redirected to guestjoin if they are a guest anyway
+            invite_link = f"https://www.where2play.net/voting/{session_pin}/"
 
             # Send email
             subject = 'You are invited to a voting session'
